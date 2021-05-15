8951ca5a10f067cb250d19ad9914546f7fe97e8e
Jonathan Oxborrow <jonathan.todd.oxborrow@gmail.com>
2021-05-15 12:44:37 -0500

    Started adding commandline options
    Started adding the directory option commandline arguement.

--- delete_files.py
+++ delete_files.py
@@ -1,8 +1,21 @@
 #! /usr/bin/env python
 
 import os
+from optparse import OptionParser
+
+# Parse all of the commandline options
+parser = OptionParser()
+parser.add_option("-d", "--directory", 
+                  dest="root_dir",
+                  type='string',
+                  help="Root directory from which to delete all files.")
+
+(options, args) = parser.parse_args()
+
+# Verify that the directory exists
 
 
+# Recurrsively delete all of the files
 for root, dirs, files in os.walk('.'):
     for name in files:
         print(os.path.join(root, name))
