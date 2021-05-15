dfb8458b4c3b6f44e34d1151d7596ecc5b8494cf
Jonathan Oxborrow <jonathan.todd.oxborrow@gmail.com>
2021-05-15 13:11:20 -0500

    Added dry run option to delete_files.py

--- delete_files.py
+++ delete_files.py
@@ -9,15 +9,23 @@ parser.add_option("-d", "--directory",
                   dest="root_dir",
                   type='string',
                   help="Root directory from which to delete all files.")
+parser.add_option("-dr", "--dry-run",
+                  action = "store_true",
+                  default = False,
+                  help = "Prints all file names to the console, instead of deleteing them.",
+                  dest="dry_run")
 
 (options, args) = parser.parse_args()
 
 # Verify that the directory exists
 
+if (options.dry_run == True):
+    # Print all of the files that will be deleted
+    for root, dirs, files in os.walk('.'):
+        for name in files:
+            print(os.path.join(root, name))
+else:
+    print("Files deleting ...")
 
-# Recurrsively delete all of the files
-for root, dirs, files in os.walk('.'):
-    for name in files:
-        print(os.path.join(root, name))
 
 
