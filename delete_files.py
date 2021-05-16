3756c517cafd76aaa55befbbac0db726ab38bb15
Jonathan Oxborrow <jonathan.todd.oxborrow@gmail.com>
2021-05-16 08:54:10 -0500

    Working script

--- delete_files.py
+++ delete_files.py
@@ -9,7 +9,7 @@ parser.add_option("-d", "--directory",
                   dest="root_dir",
                   type='string',
                   help="Root directory from which to delete all files.")
-parser.add_option("-dr", "--dry-run",
+parser.add_option("-r", "--dry-run",
                   action = "store_true",
                   default = False,
                   help = "Prints all file names to the console, instead of deleteing them.",
@@ -18,14 +18,22 @@ parser.add_option("-dr", "--dry-run",
 (options, args) = parser.parse_args()
 
 # Verify that the directory exists
+if not os.path.isdir(options.root_dir):
+    raise Exception("Not a valid directory")
 
+# Execute file printing or file deletions
 if (options.dry_run == True):
     # Print all of the files that will be deleted
-    for root, dirs, files in os.walk('.'):
+    for root, dirs, files in os.walk(options.root_dir):
         for name in files:
             print(os.path.join(root, name))
 else:
-    print("Files deleting ...")
+    # Delete the files from the root directory
+    for root, dirs, files in os.walk(options.root_dir):
+        for name in files:
+            os.remove(os.path.join(root, name))
+
+    print(f"All files deleted from {options.root_dir}")
 
 
 
